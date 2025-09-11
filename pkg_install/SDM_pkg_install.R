cb3bbc335f291af4e41d1ae1b2431bff65525c58
AMBarbosa <AMBarbosa@users.noreply.github.com>
2025-09-11 12:20:52 +0100

    square brackets around days

--- pkg_install/SDM_pkg_install.R
+++ pkg_install/SDM_pkg_install.R
@@ -6,7 +6,7 @@
 
 # after installing all packages, load each one with library() and check if there are additional errors, in which case do as above
 
-# day 1
+# [day 1]
 install.packages("terra")
 # install.packages("tidyterra")  # optional, if you want to use tidyverse functions on terra maps
 install.packages("geodata")
@@ -14,7 +14,7 @@ install.packages("jsonlite")  # required by geodata::sp_occurrence
 install.packages("modEvA")
 install.packages("fuzzySim")
 
-# day 2
+# [day 2]
 install.packages("collinear")
 install.packages("predicts")
 install.packages("maxnet")
@@ -24,12 +24,12 @@ devtools::install_github("gbm-developers/gbm3")  # if you get error "Could not f
 install.packages("randomForest")
 install.packages("corrplot")
 
-# day 3
+# [day 3]
 if (!("ggplot2" %in% installed.packages())) install.packages("ggplot2")  # required by blockCV::cv_spatial()
 install.packages("blockCV")
 install.packages("automap")  # required by blockCV::cv_spatial_autocor()
 
-# day 4
+# [day 4]
 install.packages("plotmo")
 install.packages("mop")
 install.packages("SSDM")
